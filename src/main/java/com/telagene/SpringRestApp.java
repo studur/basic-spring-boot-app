4ebdec4cd64a4e9d12f938342a03f7015abe6ede
MORISSETTE Martin <Martin.Morissette@grassvalley.com>
2020-06-12 14:55:15 -0400

    Added basic fileupload controller

--- src/main/java/com/telagene/SpringRestApp.java
+++ src/main/java/com/telagene/SpringRestApp.java
@@ -1,13 +1,29 @@
 package com.telagene;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.telagene.storage.StorageProperties;
+import com.telagene.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringRestApp {
 
    public static void main(String[] args) {
       SpringApplication.run(SpringRestApp.class, args);
    }
+
+   @Bean
+   CommandLineRunner init(StorageService storageService) {
+      return (args) -> {
+         storageService.deleteAll();
+         storageService.init();
+      };
+   }
+
 }
